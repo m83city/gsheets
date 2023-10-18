124a0554e723c608332c97312f2d2112ad76ea1a
m83city <rostorguievv@gmail.com>
2023-10-18 15:20:19 +0300

    first commit

---
+++ src/main/java/com/example/gsheets/GsheetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsheetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsheets/GsheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
