103951a3861967afcf19725a4de6e676ba9c6dd9
m83city <rostorguievv@gmail.com>
2023-11-30 13:20:09 +0200

    change path sheet/ to student/

--- src/main/java/com/example/gsheets/controller/SheetController.java
+++ src/main/java/com/example/gsheets/controller/SheetController.java
@@ -18,24 +18,20 @@ import static com.example.gsheets.service.mapper.StudentMapper.fromDTOController
 public class SheetController {
    private final SheetService service;
 
-    @GetMapping("/sheet/{id}")
+    @GetMapping("/student/{id}")
     public StudentDTOController getStudentById(@PathVariable String id) throws GeneralSecurityException, IOException {
         return fromStudentToDTOController(service.getStudentById(id));
     }
-    @PutMapping("/sheet/{id}")
-    public StudentDTOController updateStudentById (@PathVariable String id, @RequestBody StudentDTOController updatedStudent) throws GeneralSecurityException, IOException {
+    @PutMapping("/student/{id}")
+    public void updateStudentById (@PathVariable String id, @RequestBody StudentDTOController updatedStudent) throws GeneralSecurityException, IOException {
         service.updateStudent(id, fromDTOControllerToStudent(updatedStudent));
-        return null;
     }
-    @DeleteMapping("/sheet/{id}")
+    @DeleteMapping("/student/{id}")
     public void deleteStudentById(@PathVariable String id) throws GeneralSecurityException, IOException {
         service.deleteStudent(id);
     }
-    @PostMapping("/sheet")
-    public StudentDTOController createNewStudent(@RequestBody StudentDTOController student ) throws GeneralSecurityException, IOException {
+    @PostMapping("/student")
+    public void createNewStudent(@RequestBody StudentDTOController student ) throws GeneralSecurityException, IOException {
         service.createStudent(fromDTOControllerToStudent(student));
-
-        return null;
     }
-
 }
